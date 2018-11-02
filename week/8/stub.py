5c3193525e798657b72d302113cbb32d6212a871
leroi1800 <d.hislop17@gmail.com>
2018-11-02 17:08:14 -0400

    Completed Assignment

--- week/8/stub.py
+++ week/8/stub.py
@@ -2,16 +2,29 @@
 
 import sys
 import struct
-
+from datetime import datetime
 
 # You can use this method to exit on failure conditions.
 def bork(msg):
     sys.exit(msg)
 
-
+def not_ascii(inp):
+    for c in inp:
+        if c < 0 or c > 127:
+            return True
+    return False
+    
 # Some constants. You shouldn't need to change these.
 MAGIC = 0xdeadbeef
 VERSION = 1
+SECTION_PNG = 0x1
+SECTION_DWORDS = 0x2
+SECTION_UTF8 = 0x3
+SECTION_DOUBLES = 0x4
+SECTION_WORDS = 0x5
+SECTION_COORD = 0x6
+SECTION_REFERENCE = 0x7
+SECTION_ASCII = 0x9
 
 if len(sys.argv) < 2:
     sys.exit("Usage: python2 stub.py input_file.fpff")
@@ -24,7 +37,9 @@ with open(sys.argv[1], 'rb') as fpff:
 # Hint: struct.unpack will be VERY useful.
 # Hint: you might find it easier to use an index/offset variable than
 # hardcoding ranges like 0:8
-magic, version = struct.unpack("<LL", data[0:8])
+index = 0
+offset = 12
+magic, version, timestamp = struct.unpack("<LLL", data[index:offset])
 
 if magic != MAGIC:
     bork("Bad magic! Got %s, expected %s" % (hex(magic), hex(MAGIC)))
@@ -32,11 +47,108 @@ if magic != MAGIC:
 if version != VERSION:
     bork("Bad version! Got %d, expected %d" % (int(version), int(VERSION)))
 
+timestr = str(int(timestamp))
+
+if len(timestr) != 10:
+    bork("Invalid timestamp, length was not 10")
+
+index = offset
+offset += 8
+
+author = data[index:offset]
+
+if not not_ascii(author):
+    bork("Invalid author field")
+
+#dealing with the null padding
+
+index = offset
+offset += 4
+
+sec_num = struct.unpack("<L", data[index:offset])[0]
+
+if int(sec_num) < 0:
+    bork("Invalid section number")
+
 print("------- HEADER -------")
 print("MAGIC: %s" % hex(magic))
 print("VERSION: %d" % int(version))
+print("TIMESTAMP: %s" % datetime.utcfromtimestamp(int(timestr)))
+print("AUTHOR: %s" % author.decode("ascii"))
+print("SECTIONS: %d" % int(sec_num))
+
 
 # We've parsed the magic and version out for you, but you're responsible for
 # the rest of the header and the actual FPFF body. Good luck!
-
 print("-------  BODY  -------")
+
+while offset < len(data):
+    index = offset
+    offset += 8
+
+    s_type,s_len = struct.unpack("<LL", data[index:offset])
+
+    print("TYPE: %s" % int(s_type))
+    print("LENGTH: %d" % int(s_len))
+    index = offset
+    offset += s_len
+    s_t = data[index:offset]
+
+    if s_type == SECTION_PNG:
+        if not len(s_t) == s_len:
+            bork("Invalid PNG")
+        print("IMAGE Found")
+        fout = open("embedded" + str(index)+ ".png", 'w')
+        fout.write(b'\x89\x50\x4e\x47\x0d\x0a\x1a\x0a')
+        fout.write(s_t)
+        print("------------------------------")
+    elif s_type == SECTION_DWORDS:
+        if not len(s_t) % 8 == 0:
+            bork("Invalid Dwords Section")
+        arr = struct.unpack('<' + 'Q'*int(len(s_t)/8), s_t)
+        for n in arr:
+            print('%d' % int(n))
+        print("------------------------------")
+    elif s_type == SECTION_UTF8:
+        if not len(s_t) == s_len:
+            bork("Invalid UTF")
+        print("Text: %s" % s_t.decode("utf-8"))
+        print("------------------------------")
+    elif s_type == SECTION_DOUBLES:
+        if not len(s_t) % 8 == 0:
+            bork("Invalid Doubles Section")
+        arr = struct.unpack('<' + 'd'+int(len(s_t)/8), s_t)
+        for n in arr:
+            print('%f' % float(n))
+        print("------------------------------")
+    elif s_type == SECTION_WORDS:
+        if not len(s_t) % 4 ==  0:
+            bork("Invalid Words Section")
+        arr = struct.unpack('<' + 'L'*int(len(s_t)/4), s_t)
+        for n in arr:
+            print('%d' % int(n))
+        print("------------------------------")
+    elif s_type == SECTION_COORD:
+        #valid coordinates
+        lat,lon = struct.unpack('<dd', s_t)
+        if not len(s_t) == 16:
+            bork("Invalid Coordinates")
+        print('%f' % lat)
+        print('%f' % lon)
+        print("------------------------------")
+    elif s_type == SECTION_REFERENCE:
+        #must be a word
+        if not len(s_t) == 4:
+            bork("Invalid Reference")
+        arr = struct.unpack('<L', s_t)[0]
+        print(str(int(arr)))
+        print("------------------------------")
+    elif s_type == SECTION_ASCII:
+        #must be slen bytes of text
+        if not len(s_t) == s_len:
+            bork("Invalid ascii")
+        print("Text: %s" % s_t.decode("ascii"))
+        print("------------------------------")
+    else:
+        bork("invalid section")
+        print("------------------------------")
--- week/8/writeup/README.md
+++ week/8/writeup/README.md
@@ -1,39 +1,49 @@
 Writeup 8 - Forensics II, Network Analysis and File Carving/Parsing
 =====
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION HERE*
+Name: Darren Hislop
+Section: 0101
 
-I pledge on my honor that I have not given or received anyunauthorized assistance on this assignment or examination.
+I pledge on my honor that I have not given or received any unauthorized assistance on this assignment or examination.
 
-Digital acknowledgement of honor pledge: *PUT YOUR NAME HERE*
+Digital acknowledgement of honor pledge: Darren Hislop
 
 ## Assignment 8 Writeup
 
 ### Part 1 (45 Pts)
-1.
+1. Yes. They used it on csic.umiacs.umd.edu
 
-2.
+2. Their names are laz0r4hx and c0uchpot4doz
 
-3.
+3. Their IPs are 206.189.113.189 which is located in London, United Kingdom. The other is 104.248.224.85, located in New York City.
 
-4.
+4. They are using port 2749.
 
-5.
+5. It is happening the next day from when the breach occurred at 1500.
 
-6.
+6. https://drive.google.com/file/d/1McOX5WjeVHNLyTBNXqbOde7l8SAQ3DoI/view?usp=sharing. It is possible to read that file using some parser that they sent to each other.
 
-7.
+7. They expect to see each other tomorrow (after whenever the breach happened).
 
 ### Part 2 (55 Pts)
 
 *Report your answers to the questions about parsing update.fpff below.*
-1.
+1. It was generated on October 25, 2018 at 12:40:07 AM.
 
-2.
+2. laz0r4hx wrote the file.
 
-3.
+3. It says that it has 9 sections but there are actually 11 sections.
 
-4.
+4. 1) ASCII string -> Call this number to get your flag: (422) 537 - 7946 <br>
+   2) Words -> 3 1 4 1 5 9 2 6 5 3 5 8 9 7 9  <br>
+   3) Coordinates -> 38.991610,-77.027540  <br>
+   4) Reference -> 1 <br>
+   5) ASCII string -> The infamous security pr0s at CMSC389R will never find this!  <br>
+   6) ASCII string -> The first recorded uses of steganography Can be traced back to 440 BC when Herodotus Mentions two exampleS in his Histories.[3] Histicaeus s3nt a message to his vassal, Arist8goras, by sha9ving the hRead of his most trusted servan-t, "marking" the message onto his scal{p, then sending him on his way once his hair had rePrown, withl the inastructIon, "WheN thou art come to Miletus, bid _Aristagoras shave thy head, and look thereon." Additionally, demaratus sent a warning about a forthcoming attack to Greece by wrIting it dirfectly on the wooden backing oF a wax tablet before applying i_ts beeswax surFace. Wax tablets were in common use then as reusabLe writing surfAces, sometimes used for shorthand. In his work Polygraphiae Johannes Trithemius developed his so-called "Ave-Maria-Cipher" that can hide information in a Latin praise of God. "Auctor Sapientissimus Conseruans Angelica Deferat Nobis Charitas Gotentissimi Creatoris" for example contains the concealed word VICIPEDIA.[4}  <br>
+   7) Coordinates -> 38.991094, -76.932802  <br>
+   8) PNG -> A picture <br>
+   9) ASCII string -> AF(saSAdf1AD)Snz**asd1  <br>
+   10) ASCII string -> Q01TQzM4OVIte2gxZGQzbi1zM2N0MTBuLTFuLWYxbDN9  <br>
+   11) Dwords -> 4, 8, 15, 16, 23, 42  <br>
 
-5.
+5. The flags are CMSC389R-{c0rn3rst0ne_airlin3s_to_the_m00n}, CMS389R-{PlaIN_dIfF_FLAG}
